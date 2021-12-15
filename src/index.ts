f4bbc1d7ad2961c1900b826799226a79a69deba0
Quentin Lem <quentinlemcode@gmail.com>
2021-12-15 11:45:34 +0100

    Updated template

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import { ReadFile } from './fileread';
-
-const file = new ReadFile('input.txt');
-
-function onEachLine(line: string) {
-  console.log(line);
-}
-
-async function process() {
-  await file.applyFunction(onEachLine);
-}
-process().then();
+console.log("hello world");
